7e3c2a1c8e40cfe2d736645ddb72dedd74e52633
Felipe Reis <contacto.felipereis@gmail.com>
2021-04-20 23:36:48 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default Home;
